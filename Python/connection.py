6c5613b609d6348ea56050dc20a9c25f44e00e1a
TingKai <tingkaihsu0924@gmail.com>
2024-07-10 12:09:54 +0800

    connection okay

--- Python/connection.py
+++ Python/connection.py
@@ -1,41 +1,61 @@
 import serial
 import time
+
+# Function to send data
+def send_data(data):
+    time.sleep(0.1)
+    ser.write(data.encode())  # Convert string to bytes and send
+    #mes = ser.readline(64)
+    #if mes:
+    #    print(data)
+    #    print(mes.decode())
+
+# Function to receive data
+def receive_data():
+    data = ser.readline(64)  # Read a line of data
+    return data.decode()   # Convert bytes to string and return
+
+
+
+
 # Set up the serial connection
 ser = serial.Serial(
-    port='/dev/ttyUSB0',  # Device name
+    port='/dev/ttyUSB1',  # Device name
     baudrate=9600,        # Baud rate (this depends on your device)
     timeout=1             # Timeout for read
 )
 
+ser.dsrdtr = True
+time.sleep(1.0)
+print(ser)
+
 # Check if the serial port is open
-if ser.is_open:
-    print("Connection established!")
-else:
-    print("Failed to open the serial port.")
-
-# Function to send data
-def send_data(data):
-    ser.write(data.encode())  # Convert string to bytes and send
-    #time.sleep(0.1)
-    data = ser.readline(16)
-    print(data.decode().strip())
-
-# Function to receive data
-def receive_data():
-    data = ser.readline()  # Read a line of data
-    return data.decode()   # Convert bytes to string and return
+# if ser.is_open:
+#     print("Connection established!")
+#     print(ser)
+# else:
+#     print("Failed to open the serial port.")
 
 # Example code
 
-#send_data(chr(0x14))
+# send_data(chr(0x14))
 send_data('\x14')
 send_data('\x11')
-ser.write('*IDN?')
+# remember to add \r\n in front of the commend
+send_data('\r\nCLEA')
+send_data('\r\n*IDN?')
+send_data('\r\nSTAR')
 print(receive_data())
-print('*IDN?'.encode())
-print('type of chr(0x14): ', type(chr(0x14)))
-print('type of \'0x14\': ', type('0x14'))
-print(chr(0x14).encode())
-print('\x14'.encode())
+# It seems that the commend would first be stored in  buffer
+# and then be pushed to machine when the other commend comes in
+send_data('\r\nCOUN?')
+time.sleep(30.0)
+send_data('\r\nTIME?')
+print(receive_data())
+send_data('\r\nSTOP')
+
+print(receive_data())
+
 send_data(chr(0x12))
+print(ser)
 ser.close()
