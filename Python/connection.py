97d833b6376d081f54f8255d50b313286148ecf8
TingKai <tingkaihsu0924@gmail.com>
2024-07-18 11:53:56 +0800

    only counter mode

--- Python/connection.py
+++ Python/connection.py
@@ -5,9 +5,10 @@ import time
 def send_data(data):
     time.sleep(0.1)
     ser.write(data.encode())  # Convert string to bytes and send
-    # mes = ser.readline(64)
-    # if mes != b'':
-    #     print(mes.decode())
+    mes = ser.readline(64)
+    if mes != b'':
+        print(mes.decode())
+    return mes.decode()
 
 # Function to receive data
 def receive_data():
@@ -43,31 +44,45 @@ send_data('\r\nMODE?\r\n')
 events = 0
 pre_events = 0
 print('start counting!')
+print('ch1 ch2', file = file)
 try: 
     while(True):
         send_data('\r\nSTAR\r\n')
-        # COUNTER
-        time.sleep(0.1)
-        ser.write('\r\nEVTS?\r\n'.encode())
-        events = int(ser.readline(64).decode())
-        if events != pre_events:
-            print('Event: ', events)
-            send_data('\r\nCOUN?\r\n')
-            mes = receive_data()
-            if mes != '':
-                print(mes)
-                data = mes.split(";")
-                ch1 = data[0].split(",")
-                ch2 = data[1].split(",")
-                print(ch1[1], file= file, end = ' ')
-                print(ch2[1], file= file, end = '')
-            pre_events = events
-            send_data('\r\nCLEA\r\n')
-            print('start counting!')
+        # COUNTER/TIME mode
+        # time.sleep(0.1)
+        # ser.write('\r\nEVTS?\r\n'.encode())
+        # events = int(ser.readline(64).decode())
+        # if events != pre_events:
+        #     print('Event: ', events)
+        #     mes = send_data('\r\nCOUN?\r\n')
+        #     if mes != '' and mes.startswith('1'):
+        #         data = mes.split(";")
+        #         ch1 = data[0].split(",")
+        #         ch2 = data[1].split(",")
+        #         print(ch1[1], file= file, end = ' ')
+        #         print(ch2[1], file= file, end = '')
+        #     pre_events = events
+        #     send_data('\r\nCLEA\r\n')
+        #     print('start counting!')
+        time.sleep(10.0)
+        mes = send_data('\r\nCOUN?\r\n')
+        if mes != '' and mes.startswith('1'):
+            data = mes.split(";")
+            ch1 = data[0].split(",")
+            ch2 = data[1].split(",")
+            print(ch1[1], file= file, end = ' ')
+            print(ch2[1], file= file, end = '')
 except KeyboardInterrupt:
-        print('\nKeyboard interrupt!')
-finally:
+    print('\nKeyboard interrupt!')
+    mes = send_data('\r\nCOUN?\r\n')
+    if mes != '' and mes.startswith('1'):
+        data = mes.split(";")
+        ch1 = data[0].split(",")
+        ch2 = data[1].split(",")
+        print(ch1[1], file= file, end = ' ')
+        print(ch2[1], file= file, end = '')
     send_data('\r\nSTOP\r\n')
+finally:
     send_data(chr(0x13))
     send_data(chr(0x12))
     print(ser)
