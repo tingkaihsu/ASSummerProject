a888313e9fbea0c7d47fb78ad265fc1f9a39623a
TK <tingkaihsu0924@gmail.com>
2024-07-22 23:40:30 +0800

    histogram in cpp

--- C/ROOTRead.cpp
+++ C/ROOTRead.cpp
@@ -24,6 +24,7 @@ public:
         }
         this->ch1 = stoi(ch1);
         this->ch2 = stoi(ch2);
+        this->eff = (float)this->ch2 / (float)this->ch1;
         return;
     }
     int getch1() {
@@ -32,10 +33,14 @@ public:
     int getch2() {
         return ch2;
     }
+    float geteff() {
+        return eff;
+    }
 
 private:
     int ch1 = 0;
     int ch2 = 0;
+    float eff = 0;
 };
 
 void ROOTRead() {
@@ -43,17 +48,24 @@ void ROOTRead() {
     myfile.open("data.txt");
     string mystring;
     CHA arr[10000];
-    int idx = -1;
+    int idx = -1;   //the first line is ch1 and ch2
 
     while (getline(myfile, mystring)) {
         // cout << mystring << endl;
         if (idx >= 0) {
             arr[idx].getdata(mystring);
-            cout << arr[idx].getch1() << " " << arr[idx].getch2() << endl;
+            // cout << arr[idx].getch1() << " " << arr[idx].getch2() << endl;
         }
         ++idx;
     }
 
     myfile.close();
+
+    TH1* h_eff = new TH1D("eff", "Histogram of Effeciency", 100,0.0,1.0);
+    
+    for (int i = 0; i < size(arr); ++i) {
+        h_eff->Fill(arr[i].geteff());
+    }
+    h_eff->Draw("bar1");
     return;
 }
