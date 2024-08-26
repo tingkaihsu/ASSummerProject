8d3f7634467010f0ae93c642026c5559445a9350
TingKai <tingkaihsu0924@gmail.com>
2024-08-26 14:00:05 +0800

    renew stuff

--- Python/connection.py
+++ Python/connection.py
@@ -28,7 +28,7 @@ ser = serial.Serial(
 # Open the flow control
 ser.dsrdtr = True
 time.sleep(1.0)
-print(ser)
+print(ser, file = file)
 
 # Example code
 
@@ -38,58 +38,30 @@ send_data('\x11')
 # remember to add \r\n in sandwich form of the commend
 send_data('\r\nCLEA\r\n')
 send_data('\r\nEVTS\r\n')
-send_data('\r\n*IDN?\r\n')
-print('current mode: ', end = '')
-send_data('\r\nMODE?\r\n')
+idn = send_data('\r\n*IDN?\r\n')
+print(idn, file = file)
 events = 0
 pre_events = 0
-print('start counting!')
-print('ch1 ch2', file = file)
+print('start counting!', file = file)
+print('# ch1 ch2', file = file)
+
+initime = time.time()
 try: 
     while(True):
         send_data('\r\nSTAR\r\n')
-        # COUNTER/TIME mode
-        # time.sleep(0.1)
-        # ser.write('\r\nEVTS?\r\n'.encode())
-        # events = int(ser.readline(64).decode())
-        # if events != pre_events:
-        #     print('Event: ', events)
-        #     mes = send_data('\r\nCOUN?\r\n')
-        #     if mes != '' and mes.startswith('1'):
-        #         data = mes.split(";")
-        #         ch1 = data[0].split(",")
-        #         ch2 = data[1].split(",")
-        #         print(ch1[1], file= file, end = ' ')
-        #         print(ch2[1], file= file, end = '')
-        #     pre_events = events
-        #     send_data('\r\nCLEA\r\n')
-        #     print('start counting!')
-<<<<<<< HEAD
-        time.sleep(300.0)
-=======
-        time.sleep(600.0)
->>>>>>> 1d4a6603a02200f54668af908dcd98f43a676cd9
+        time.sleep(10.0)
         mes = send_data('\r\nCOUN?\r\n')
         if mes != '' and mes.startswith('1'):
             data = mes.split(";")
             ch1 = data[0].split(",")
             ch2 = data[1].split(",")
-            print("current time: ", time.time(), file=file)
             print(ch1[1], file= file, end = ' ')
             print(ch2[1], file= file, end = '')
 except KeyboardInterrupt:
-    print('\nKeyboard interrupt!')
-    mes = send_data('\r\nCOUN?\r\n')
-    if mes != '' and mes.startswith('1'):
-        data = mes.split(";")
-        ch1 = data[0].split(",")
-        ch2 = data[1].split(",")
-        print(ch1[1], file= file, end = ' ')
-        print(ch2[1], file= file, end = '')
+    print('\nKeyboard interrupt!', file = file)
     send_data('\r\nSTOP\r\n')
-    print("current time: ", time.time(), file=file)
 finally:
     send_data(chr(0x13))
     send_data(chr(0x12))
-    print(ser)
+    print(ser, file = file)
     ser.close()
